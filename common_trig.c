2072e776ad6982ce85de6f0e106dbc65c3088677
Ozzywtlk <89253167+Ozzywtlk@users.noreply.github.com>
2025-05-08 14:00:05 +0200

    Add files via upload

---
+++ common_trig.c
@@ -0,0 +1,12 @@
+#include "common_trig.h"
+
+// Definitions are ONLY here
+double cot(double z) {
+    return 1.0 / tan(z);
+}
+double sec(double z) {
+    return 1.0 / cos(z);
+}
+double csc(double z) {
+    return 1.0 / sin(z);
+}
---
+++ common_trig.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_TRIG_H
+#define COMMON_TRIG_H
+#include <math.h>
+#include <complex.h>
+#define M_PI 3.14159265358979323846
+double cot(double z);
+double sec(double z);
+double csc(double z);
+#endif // COMMON_TRIG_H
